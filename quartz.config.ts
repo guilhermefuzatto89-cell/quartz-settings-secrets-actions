f1541874ce8b80a35e5afdd2c258173fe34b6590
Guilherme Fuzatto de Oliveira <guilhermefuzattodeoliveira@Mac-mini-de-Guilherme.local>
2026-05-18 17:29:55 -0300

    ci: deploy automático + baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "guilherme.ppg.br",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
